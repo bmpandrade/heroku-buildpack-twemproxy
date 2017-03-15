33baf025c785f6043e66a7a19d2113cbfdb7ac09
Eric Hough <eric@tubepress.com>
2017-03-15 11:02:55 -0700

    Rebuild config file at each invocation

--- bin/gen-twemproxy-conf.sh
+++ bin/gen-twemproxy-conf.sh
@@ -3,6 +3,8 @@
 REDIS_URLS=${TWEMPROXY_URLS:-REDISCLOUD_URL}
 n=1
 
+rm -f /app/vendor/twemproxy/twemproxy.yml
+
 for _REDIS_URL in $REDIS_URLS
 do
   echo "Setting ${_REDIS_URL}_TWEMPROXY config var"
